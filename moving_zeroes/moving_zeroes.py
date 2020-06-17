73597ed20df51ced7700764c03c9ebb8b420e483
skredenmathias <skredenmathias@users.noreply.github.com>
2020-06-18 00:31:27 +0200

    single number, moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,11 +3,13 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
-
-
+    lst = []
+    for i in arr:
+        if i == 0:
+            lst.append(i)
+        else:
+            lst.insert(0, i)
+    return lst
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,31 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+arrA = [1,1,2,2,3,4,4]
 def single_number(arr):
     # Your code here
+    for elem in arr:
+        if arr.count(elem) == 1:
+            return elem
 
-    pass
+    
+
+
+    # OR:
+    # create list, input all numbers that are duplicates
+    # lst = []
+    # for elem in range(len(arr)):
+    #     for j in range(elem+1, len(arr)):
+    #         if arr[elem] == arr[j]:
+    #             lst.append(arr[elem])
+    # check which element of arr does not appear in new list
+    # return that element
+    # for i in lst: #? reminder: confused about when to use lst, range(), len()
+    #     for j in arr:
+    #         if i != j:
+    #             return j
+
+# print(single_number(arrA))
 
 
 if __name__ == '__main__':
